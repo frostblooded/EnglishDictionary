b507d77361985e6c1cdfdf0bd20ea56de3f34e1d
Nikolay Danailov <frostblooded@yahoo.com>
2015-06-13 17:38:35 +0300

    removed a useless function

--- scripts/getWord.php
+++ scripts/getWord.php
@@ -2,7 +2,7 @@
 
 $word = strtolower($_POST["word"]); //because the words are in lower case
 
-$json_path = "../gcide/gcide_" . strtolower($word[0]) . "-entries.json";
+$json_path = "../gcide/gcide_" . $word[0] . "-entries.json";
 $json_text = file_get_contents($json_path);
 $json = json_decode($json_text, true);
 
