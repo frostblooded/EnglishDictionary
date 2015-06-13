d375356c58835ec34644a9294ffcb8dcbe2e710d
Nikolay Danailov <frostblooded@yahoo.com>
2015-06-13 13:04:01 +0300

    Made all meaning appear and in a better format

--- scripts/getWord.php
+++ scripts/getWord.php
@@ -5,8 +5,8 @@ $json_path = "../gcide/gcide_" . strtolower($word[0]) . "-entries.json";
 $json_text = file_get_contents($json_path);
 $json = json_decode($json_text, true);
 
-$result = $json[$word]['definitions'][0]['definition'];
+$result = $json[$word]['definitions'];
 
-echo $result;
+echo json_encode($result);
 
 ?>
--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,29 @@ $(document).ready( function () {
             url: "scripts/getWord.php",
             data: formdata,
             success: function(response){
-                $("body").append("<div>" + response + "</div>");
+                $json = JSON.parse(response);
+
+                
+                for(var i = 0; i < $json.length; i++){
+                    $div = $("<div class='result'></div>");
+                    
+                    $div.append("<div>Meaning " + (i + 1) + ":</div>");
+                    
+                    $div.append("<div class='part_of_speech'>Part of speech:" + $json[i].part_of_speech + "</div>");
+                    
+                    if($json[i].field != ""){
+                        $div.append("<div class='field'>Field: " + $json[i].field + "</div>");
+                    }
+                    
+                    $div.append("<div class='definition'> Definition: " + $json[i].definition + "</div>");
+                    
+                    $div.append("<br>");
+                    $div.append("<br>");
+                    
+                    $("body").append($div);
+                }
+                
+                
             },
             error: function(error){
             }
