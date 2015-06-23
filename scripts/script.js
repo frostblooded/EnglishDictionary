3f877db2b9f3ee9c4fc81ef65f92e3c3ae3a175d
Nikolay Danailov <frostblooded@yahoo.com>
2015-06-23 21:40:28 +0300

    Cleaned up code

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
+var input_text;
+
 function Shhhhh(input){
-       return input.toLocaleLowerCase() === "kawaii";
+       return input_text.toLocaleLowerCase() === "kawaii";
 }
 
 function ShowError(error_message){
@@ -21,11 +23,51 @@ function ResetInput(){
     $input.focus();
 }
 
+function HandleAJAXSuccess(response){
+    try{
+        $json = JSON.parse(response);
+
+        ClearArea();
+
+        $(".results").append("<div class='searched_word'>Searched word: <span class='searched_word_text'>" + input_text.toUpperCase() + "</span></div>");
+
+        $result = $("<div class='result'></div>");
+
+        for(var i = 0; i < $json.length; i++){
+            $result = $("<div class='result'></div>");
+            $result.append("<div class='meaning'>Meaning " + (i + 1) + ":</div>");
+
+            if($json[i].part_of_speech){
+                $result.append("<div class='part_of_speech'>Part of speech: " + $json[i].part_of_speech + "</div>");
+            }
+
+            if($json[i].field){
+                $result.append("<div class='field'>Field: " + $json[i].field + "</div>");
+            }
+
+            $result.append("<div class='definition'>Definition: " + $json[i].definition + "</div>");
+
+            $(".results").append($result);
+        }
+
+        ResetInput();
+    }
+    catch(error){
+        /* the error might say there is an unexpected token, but in truth the returned response
+           is empty and thus the parsing of the json is failing */
+        ShowError("The entered input is invalid or the word doesn't exist in our database!");
+    }
+}
+
+function HandleAJAXError(error){
+    ShowError("The page is not connected to the server!");
+}
+
 $(document).ready(function(){
     $(".input").focus();
     
     $('form').submit(function(){
-        var input_text = $(".input").val();
+        input_text = $(".input").val();
         
         if(Shhhhh(input_text)){
             ClearArea();
@@ -33,46 +75,13 @@ $(document).ready(function(){
         }
         else{
             var formdata = $(this).serialize();
+            
             $.ajax({
                 type: "POST",
                 url: "scripts/getWord.php",
                 data: formdata,
-                success: function(response){
-                    try{
-                        $json = JSON.parse(response);
-
-                        ClearArea();
-
-                        $(".results").append("<div class='searched_word'>Searched word: <span class='searched_word_text'>" + input_text.toUpperCase() + "</span></div>");
-
-                        $result = $("<div class='result'></div>");
-
-                        for(var i = 0; i < $json.length; i++){
-                            $result = $("<div class='result'></div>");
-                            $result.append("<div class='meaning'>Meaning " + (i + 1) + ":</div>");
-
-                            if($json[i].part_of_speech){
-                                $result.append("<div class='part_of_speech'>Part of speech: " + $json[i].part_of_speech + "</div>");
-                            }
-
-                            if($json[i].field){
-                                $result.append("<div class='field'>Field: " + $json[i].field + "</div>");
-                            }
-
-                            $result.append("<div class='definition'>Definition: " + $json[i].definition + "</div>");
-
-                            $(".results").append($result);
-                        }
-
-                        ResetInput();
-                    }
-                    catch(error){
-                        ShowError("The entered input is invalid or the word doesn't exist in our database!");
-                    }
-                },
-                error: function(error){
-                    ShowError("The page is not connected to the server!");
-                }
+                success: HandleAJAXSuccess,
+                error: HandleAJAXError
             });
         }
         
